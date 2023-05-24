7d7a75b1d9aad09e453e71fefb84200881842266
YebinKim98 <dpqls9898@gmail.com>
2023-05-24 15:32:06 +0900

    2023-05-24

---
+++ src/LoopFors.java
@@ -0,0 +1,14 @@
+public class LoopFors {
+    public static void main(String[] args) {
+        // for (statement 1;, statement 2; statement 3;) {
+        // cod block to executed
+        // }
+        int first = 10;
+        for (int first = 5; first < second; first = first + 2) {
+           // cod block to executed
+           System.out.println(first);
+        }
+        System.out.println();
+        // return 0;
+    }
+}
---
+++ src/LoopWithMark.java
@@ -0,0 +1,13 @@
+public class LoopWithMark {
+    public static void main(String[] args) {
+        // for (statement 1;, statement 2; statement 3;) {
+        // cod block to executed
+        // }
+        for (int line = 1; line < 5; line = line + 1) {
+            // cod block to executed
+            System.out.println("for" + line); 
+        }
+        System.out.println("end");
+        // return 0; 
+    }
+}
