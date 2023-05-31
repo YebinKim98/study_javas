e496e478438bc228f3f924cdd60ed422920023e6
YebinKim98 <dpqls9898@gmail.com>
2023-05-31 11:48:42 +0900

    git

--- src/GitConflicts.java
+++ src/GitConflicts.java
@@ -1,8 +1,9 @@
 public class GitConflicts {
     public static void main(String[] args) {
         try {
+            int second = 1;
             int first = 0;
-            first = first + 1;
+            second = first + 1;
         } catch (Exception e) {
             // TODO: handle exception
         }
